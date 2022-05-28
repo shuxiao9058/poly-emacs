e739db73cd7781517c45c24421d9a26cd400a688
Aaron Ji <shuxiao9058@gmail.com>
2022-05-28 20:56:03 +0800

    update

--- early-init.el
+++ early-init.el
@@ -43,12 +43,12 @@ If FORCE-TANGLE is non-nil, always tangle before load."
    (expand-file-name "private_template.org" user-emacs-directory)
    (expand-file-name "private.org" user-emacs-directory)))
 
-;; (setq warning-minimum-level :debug)
-;; (setq debug-on-error t)
-;; (setq stack-trace-on-error t)
+(setq warning-minimum-level :debug)
+(setq debug-on-error t)
+(setq stack-trace-on-error t)
 
-(setq warning-minimum-level :emergency)
-(setq debug-on-error nil)
+;; (setq warning-minimum-level :emergency)
+;; (setq debug-on-error nil)
 
 ;; Native compilation settings
 (when (featurep 'native-compile)
--- init.el
+++ init.el
@@ -49,14 +49,3 @@
 (poly-load-org-config "programming")
 (poly-load-org-config "addons" t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  )
